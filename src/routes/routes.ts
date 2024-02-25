eeda05c2529a3e2c6b372897003519de3a75d649
TechforEverybody <Pankajsinghat95@gmail.com>
2024-02-25 16:20:49 +0530

    updated

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,17 +1,8 @@
 export const routes={
     ROOT:'/',
-    ABOUT:'/about',
-    CONTACT:'/contact',
-    LOGIN:'/login',
-    REGISTER:'/register',
-    PROFILE:{
-        ROOT:'/profile',
-        EDIT:'/profile/edit',
-    },
-    PRODUCT:{
-        ROOT:'/product',
-        DETAIL:'/product/:id',
-    },
+
+
+
     NOTFOUND:'/404',
     WILDCARD:"*"
 }
